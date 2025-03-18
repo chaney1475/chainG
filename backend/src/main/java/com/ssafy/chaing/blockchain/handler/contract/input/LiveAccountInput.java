e83f7785f43e9a3d8be60e1eaa65abc71bffc7fe
김동열 <ehfql6363@naver.com>
2025-03-18 17:52:07 +0900

    Add: 블록체인 컨트랙트 입출력 관련 DTO 및 CustomGasProvider 추가
    - ContractOutput, ContractOverviewOutput, ContractRentOutput, ContractUtilityOutput, PaymentInfoOutput 등 입출력 DTO 추가
    - CustomGasProvider 클래스 추가 및 사용자 정의 가스 가격 설정
    - LiveAccountInput, LiveAccountOutput 클래스 추가

---
+++ backend/src/main/java/com/ssafy/chaing/blockchain/handler/contract/input/LiveAccountInput.java
@@ -0,0 +1,8 @@
+package com.ssafy.chaing.blockchain.handler.contract.input;
+
+import lombok.Getter;
+
+@Getter
+public class LiveAccountInput {
+    String liveAccountNo;
+}
---
+++ backend/src/main/java/com/ssafy/chaing/blockchain/handler/contract/output/LiveAccountOutput.java
@@ -0,0 +1,8 @@
+package com.ssafy.chaing.blockchain.handler.contract.output;
+
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor
+public class LiveAccountOutput {
+    String liveAccountNo;
+}
