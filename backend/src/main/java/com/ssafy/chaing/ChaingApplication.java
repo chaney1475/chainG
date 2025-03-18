169e56f133239465ffae76b0413d05611c5705af
서채연 <chaney11@naver.com>
2025-03-18 15:00:12 +0900

    Update backend/src/main/java/com/ssafy/chaing/ChaingApplication.java

--- backend/src/main/java/com/ssafy/chaing/ChaingApplication.java
+++ backend/src/main/java/com/ssafy/chaing/ChaingApplication.java
@@ -3,6 +3,7 @@ package com.ssafy.chaing;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@EnableJpaAuditing
 @SpringBootApplication
 public class ChaingApplication {
 
