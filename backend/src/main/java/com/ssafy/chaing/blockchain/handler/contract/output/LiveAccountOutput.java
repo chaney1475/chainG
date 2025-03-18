937a47d052611e91c2fbfa6a3d0e13cc77eac85d
김동열 <ehfql6363@naver.com>
2025-03-19 00:20:21 +0900

    Feat: LiveAccountInput 및 LiveAccountOutput 클래스에 Getter 추가
    - LiveAccountInput 클래스에 @AllArgsConstructor 추가
    - LiveAccountOutput 클래스에 @Getter 추가

--- backend/src/main/java/com/ssafy/chaing/blockchain/handler/contract/output/LiveAccountOutput.java
+++ backend/src/main/java/com/ssafy/chaing/blockchain/handler/contract/output/LiveAccountOutput.java
@@ -1,8 +1,10 @@
 package com.ssafy.chaing.blockchain.handler.contract.output;
 
 import lombok.AllArgsConstructor;
+import lombok.Getter;
 
 @AllArgsConstructor
+@Getter
 public class LiveAccountOutput {
     String liveAccountNo;
 }
