c9369fe35911f368f42399d9465ec8671e33ce13
leedonghyun <skydh507@gmail.com>
2025-03-18 20:21:34 +0900

    fix : 인자 없는 생성자 수정

--- backend/src/main/java/com/ssafy/chaing/group/controller/request/JoinGroupRequest.java
+++ backend/src/main/java/com/ssafy/chaing/group/controller/request/JoinGroupRequest.java
@@ -2,11 +2,13 @@ package com.ssafy.chaing.group.controller.request;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
 @AllArgsConstructor
+@NoArgsConstructor
 public class JoinGroupRequest {
     private Long groupId;
     private String nickname;
