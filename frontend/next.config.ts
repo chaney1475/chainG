18d7572a75fe8bc67e956b69d4d68a9929b6ebd8
DESKTOP-4QCTMKG\SSAFY <jemilykoo@gmail.com>
2025-03-20 09:56:12 +0900

    init: default setting

---
+++ frontend/next.config.ts
@@ -0,0 +1,11 @@
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  /* config options here */
+};
+
+const withPWA = require('next-pwa')({
+  dest: 'public'
+})
+
+module.exports = withPWA(nextConfig)
