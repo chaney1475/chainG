ba5d620170d33b00a41f7066e0acd9ddb795927d
DESKTOP-4QCTMKG\SSAFY <jemilykoo@gmail.com>
2025-03-21 18:50:02 +0900

    feat: set firebse

---
+++ frontend/src/apis/auth.ts
@@ -0,0 +1,71 @@
+import axios from 'axios'
+
+// import { handleDefaultError } from '@/utils/error/handleDefaultError'
+
+interface Login {
+  accessToken: string
+}
+
+const authApi = axios.create({
+  baseURL: 'http://chaing.site',
+  timeout: 5000,
+  headers: {
+    'Content-Type': 'application/json',
+  },
+})
+
+authApi.interceptors.request.use(
+  (config) => {
+    console.log('📤 요청 보냄:', config.url)
+    return config
+  },
+  (error) => {
+    console.error('🚨 요청 오류:', error)
+    return Promise.reject(error)
+  },
+)
+
+authApi.interceptors.response.use(
+  (response) => response.data,
+  (error) => {
+    return handleDefaultError(error)
+  },
+)
+
+export default authApi
+
+export const socialLogin = async (params: Login) => {
+  try {
+    const response = await authApi.post('/auth/login/kakao', params)
+    return response.data
+  } catch (error) {
+    return null
+  }
+}
+
+// export const login = async (params: Login) => {
+//   try {
+//     const response = await authApi.post('/auth/login/kakao', params)
+//     return response.data
+//   } catch (error) {
+//     return null
+//   }
+// }
+
+export const signUp = async (params: Login) => {
+  try {
+    const response = await authApi.post('/apis/v1/auth/signup', params)
+    return response.data
+  } catch (error) {
+    return null
+  }
+}
+
+// export const insertFCMToken = async (params: { token: string }) => {
+//   try {
+//     const response = await api.post('/fcm', params)
+//     return response
+//   } catch (error) {
+//     return null
+//   }
+// }
--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,13 +1,40 @@
+'use client'
+
+import { useEffect } from 'react'
+
+import { useRouter } from 'next/navigation'
+
+import { getFcmToken, onForegroundMessage } from './firebase'
 import styles from './page.module.css'
 
 export default function Home() {
+  const router = useRouter()
+  const testValue = process.env.NEXT_PUBLIC_TEST_VALUE
+
+  useEffect(() => {
+    const initFirebase = async () => {
+      await getFcmToken()
+      onForegroundMessage()
+    }
+
+    initFirebase()
+  }, [])
+
+  useEffect(() => {
+    // TODO: 실제 로그인 상태 체크 로직 구현
+    const isLoggedIn = false // 임시로 false로 설정
+    if (!isLoggedIn) {
+      router.push('/login')
+    }
+  }, [router])
+
   return (
     <div className={styles.page}>
       <main className={styles.main}>
         <ol>
           <li>
             ChainG 인덱스 화면입니다. 로그인 상태면 로그인 페이지를 보여주고
-            아니면 홈으로 이동합니다.
+            아니면 홈으로 이동합니다. testValue 변수의 값은 {testValue}입니다.
           </li>
         </ol>
       </main>
