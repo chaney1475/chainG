17e21507e5e4929e70a90edc766ab2c992019202
leedonghyun <skydh507@gmail.com>
2025-03-24 02:25:20 +0900

    Feat : groupId에 해당 하는 당번 조회

--- backend/src/main/java/com/ssafy/chaing/duty/repository/DutyRepository.java
+++ backend/src/main/java/com/ssafy/chaing/duty/repository/DutyRepository.java
@@ -1,7 +1,9 @@
 package com.ssafy.chaing.duty.repository;
 
 import com.ssafy.chaing.duty.domain.DutyEntity;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface DutyRepository extends JpaRepository<DutyEntity, Long> {
+    List<DutyEntity> findByGroup_Id(Long groupId);
 }
