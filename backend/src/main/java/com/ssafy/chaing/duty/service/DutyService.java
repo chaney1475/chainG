2f65499f1a9972b16eb1d45a9afefe1751311749
leedonghyun <skydh507@gmail.com>
2025-03-24 21:04:23 +0900

    Refactor : 삭제된 dto import 삭제

--- backend/src/main/java/com/ssafy/chaing/duty/service/DutyService.java
+++ backend/src/main/java/com/ssafy/chaing/duty/service/DutyService.java
@@ -4,10 +4,6 @@ import com.ssafy.chaing.duty.controller.request.DutyFormRequest;
 import com.ssafy.chaing.duty.controller.response.DutyDetailResponse;
 import com.ssafy.chaing.duty.controller.response.DutyListResponse;
 import com.ssafy.chaing.duty.controller.response.RemovedDutyResponse;
-import com.ssafy.chaing.duty.service.dto.DutyFormDTO;
-import com.ssafy.chaing.duty.service.dto.DutyListDTO;
-import com.ssafy.chaing.duty.service.dto.RemovedDutyDTO;
-
 public interface DutyService {
     DutyListResponse getDuties(Long groupId);
 
