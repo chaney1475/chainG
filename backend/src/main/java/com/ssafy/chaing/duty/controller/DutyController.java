9786f302f97684cb89813c9d25b1b6b250083755
leedonghyun <skydh507@gmail.com>
2025-03-24 12:34:36 +0900

    Feat :  Duty API Controller 생성

--- backend/src/main/java/com/ssafy/chaing/duty/controller/DutyController.java
+++ backend/src/main/java/com/ssafy/chaing/duty/controller/DutyController.java
@@ -1,5 +1,6 @@
 package com.ssafy.chaing.duty.controller;
 
+import com.ssafy.chaing.auth.domain.UserPrincipal;
 import com.ssafy.chaing.common.schema.BaseResponse;
 import com.ssafy.chaing.duty.controller.request.DutyFormRequest;
 import com.ssafy.chaing.duty.controller.response.DutyDetailResponse;
@@ -9,6 +10,7 @@ import com.ssafy.chaing.duty.service.DutyService;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
@@ -16,6 +18,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
 @Tag(
         name = "Duty Controller",
         description = "당번 관리"
@@ -27,26 +30,34 @@ public class DutyController {
     private final DutyService dutyService;
 
     @GetMapping("/{groupId}")
-    public ResponseEntity<BaseResponse<DutyListResponse>> getDuties(@PathVariable Long groupId){
-        return ResponseEntity.ok(BaseResponse.success(null));
-
+    public ResponseEntity<BaseResponse<DutyListResponse>> getDuties(@PathVariable Long groupId) {
+        DutyListResponse duties = dutyService.getDuties(groupId);
+        return ResponseEntity.ok(BaseResponse.success(duties));
     }
 
     // 생성, 수정, 삭제 로직에선 Command 사용하지 않음.
     @PostMapping("/{groupId}")
-    public ResponseEntity<BaseResponse<DutyDetailResponse>> createDuty(@PathVariable Long groupId , @RequestBody DutyFormRequest body ){
-        return ResponseEntity.ok(BaseResponse.success(null));
+    public ResponseEntity<BaseResponse<DutyDetailResponse>> createDuty(@PathVariable Long groupId,
+                                                                       @AuthenticationPrincipal UserPrincipal principal,
+                                                                       @RequestBody DutyFormRequest body) {
+        DutyDetailResponse dutyDetailResponse = dutyService.creatDuty(groupId, body);
+        return ResponseEntity.ok(BaseResponse.success(dutyDetailResponse));
 
     }
 
     @PatchMapping("/{dutyId}")
-    public ResponseEntity<BaseResponse<DutyDetailResponse>> modifyDuty(@PathVariable Long dutyId, @RequestBody DutyFormRequest body){
-        return ResponseEntity.ok(BaseResponse.success(null));
+    public ResponseEntity<BaseResponse<DutyDetailResponse>> modifyDuty(@PathVariable Long dutyId,
+                                                                       @AuthenticationPrincipal UserPrincipal principal,
+                                                                       @RequestBody DutyFormRequest body) {
+
+        DutyDetailResponse dutyDetailResponse = dutyService.updateDuty(dutyId, body);
+        return ResponseEntity.ok(BaseResponse.success(dutyDetailResponse));
     }
 
     @DeleteMapping("/{dutyId}")
-    public ResponseEntity<BaseResponse<RemovedDutyResponse>> deleteDuty(@PathVariable Long dutyId) {
-
-        return ResponseEntity.ok(BaseResponse.success(null));
+    public ResponseEntity<BaseResponse<RemovedDutyResponse>> deleteDuty(@PathVariable Long dutyId,
+                                                                        @AuthenticationPrincipal UserPrincipal principal) {
+        RemovedDutyResponse removedDutyResponse = dutyService.removeDuty(dutyId);
+        return ResponseEntity.ok(BaseResponse.success(removedDutyResponse));
     }
 }
