1359692fa90305224aa360ccebad99eba46554a8
DESKTOP-4QCTMKG\SSAFY <jemilykoo@gmail.com>
2025-03-25 17:28:21 +0900

    feat: add profileList

---
+++ frontend/src/constants/profileList.ts
@@ -0,0 +1,12 @@
+export const profileList = [
+  { id: 'user01', src: '/images/profile/user01.png' },
+  { id: 'user02', src: '/images/profile/user02.png' },
+  { id: 'user03', src: '/images/profile/user03.png' },
+  { id: 'user04', src: '/images/profile/user04.png' },
+  { id: 'user05', src: '/images/profile/user05.png' },
+  { id: 'user06', src: '/images/profile/user06.png' },
+  { id: 'user07', src: '/images/profile/user07.png' },
+  { id: 'user08', src: '/images/profile/user08.png' },
+  { id: 'user09', src: '/images/profile/user09.png' },
+  { id: 'user10', src: '/images/profile/user10.png' },
+]
