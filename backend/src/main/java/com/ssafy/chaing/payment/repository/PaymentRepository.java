67d82e9688061dfe665f39c3cef3af7710ba43b2
김동열 <ehfql6363@naver.com>
2025-03-25 17:43:24 +0900

    Feat: 계약 ID와 수수료 유형으로 결제 내역 조회 기능 추가
    - findALlByContractIdAndFeeType 메서드 추가

--- backend/src/main/java/com/ssafy/chaing/payment/repository/PaymentRepository.java
+++ backend/src/main/java/com/ssafy/chaing/payment/repository/PaymentRepository.java
@@ -1,5 +1,6 @@
 package com.ssafy.chaing.payment.repository;
 
+import com.ssafy.chaing.payment.domain.FeeType;
 import com.ssafy.chaing.payment.domain.PaymentEntity;
 import com.ssafy.chaing.payment.domain.PaymentStatus;
 import java.util.List;
@@ -10,4 +11,6 @@ public interface PaymentRepository extends JpaRepository<PaymentEntity, Long> {
     List<PaymentEntity> findByStatusIn(List<PaymentStatus> started);
 
     List<PaymentEntity> findByStatus(PaymentStatus paymentStatus);
+
+    List<PaymentEntity> findALlByContractIdAndFeeType(Long contract_id, FeeType feeType);
 }
