e2ded750e18600397674d67e52591f19ed18e9b0
leedonghyun <skydh507@gmail.com>
2025-03-27 17:17:29 +0900

    Feat : 월세 공과금 계좌 에러코드 추가

--- backend/src/main/java/com/ssafy/chaing/common/exception/ExceptionCode.java
+++ backend/src/main/java/com/ssafy/chaing/common/exception/ExceptionCode.java
@@ -33,7 +33,8 @@ public enum ExceptionCode {
     USER_NOT_IN_GROUP("USER_NOT_IN_GROUP", "해당 유저가 그룹에 속해 있지 않습니다."),
 
     USER_PAYMENT_NOT_FOUND("USER_PAYMENT_NOT_FOUND", "결제 정보에 해당하는 사용자가 없습니다."),
-    LIVING_ACCOUNT_ALREADY_EXIST("LIVING_ACCOUNT_ALREADY_EXIST", "이미 계좌가 존재합니다.");
+    LIVING_ACCOUNT_ALREADY_EXIST("LIVING_ACCOUNT_ALREADY_EXIST", "이미 계좌가 존재합니다."),
+    RENT_ACCOUNT_ALREADY_EXIST("RENT_ACCOUNT_ALREADY_EXIST", "공과금/월세 계좌번호가 존재하지 않습니다.");
 
     private final String code;
     private final String message;
