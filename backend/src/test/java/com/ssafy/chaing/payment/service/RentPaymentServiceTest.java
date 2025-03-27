0c42aad8d500003560aebc62c1315239a1564295
김동열 <ehfql6363@naver.com>
2025-03-27 12:31:12 +0900

    Test: RentPaymentServiceTest 클래스 이름 수정
    - PaymentServiceImpl_RetrieveRentTest에서 RentPaymentServiceTest로 클래스 이름 변경

--- backend/src/test/java/com/ssafy/chaing/payment/service/RentPaymentServiceTest.java
+++ backend/src/test/java/com/ssafy/chaing/payment/service/RentPaymentServiceTest.java
@@ -44,7 +44,7 @@ import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 @ExtendWith(MockitoExtension.class)
-public class PaymentServiceImpl_RetrieveRentTest {
+public class RentPaymentServiceTest {
 
     @Mock
     private PaymentRepository paymentRepository;
