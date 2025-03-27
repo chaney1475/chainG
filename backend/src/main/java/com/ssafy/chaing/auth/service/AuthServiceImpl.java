8317d1f0d2a6ce37851f62d536951010391afc8d
leedonghyun <skydh507@gmail.com>
2025-03-27 12:14:01 +0900

    test : ci 충돌 테스트

--- backend/src/main/java/com/ssafy/chaing/auth/service/AuthServiceImpl.java
+++ backend/src/main/java/com/ssafy/chaing/auth/service/AuthServiceImpl.java
@@ -98,5 +98,6 @@ public class AuthServiceImpl implements AuthService {
                 .orElseThrow(() -> new NotFoundException(ExceptionCode.USER_NOT_FOUND));
 
         user.setFcmToken(command.getFcmToken());
+        
     }
 }
