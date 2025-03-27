abfe1054f065431bce4438c92db0e6c443e2c302
DESKTOP-4QCTMKG\SSAFY <jemilykoo@gmail.com>
2025-03-27 18:13:21 +0900

    feat: add home page

--- frontend/src/features/home/HomePage/index.tsx
+++ frontend/src/features/home/HomePage/index.tsx
@@ -1,16 +1,21 @@
 'use client'
 
 import { useEffect, useState } from 'react'
+import { useTranslation } from 'react-i18next'
 import { useSelector } from 'react-redux'
 
 import { useRouter } from 'next/navigation'
 
 import { getFcmToken, onForegroundMessage } from '@/app/firebase'
+import { IconButton } from '@/components/IconButton'
 import { RootState } from '@/store/store'
+import { Container, Main } from '@/styles/styles'
 
-import { Main, Page } from './styles'
+import { HomeLayout } from '../components/HomeLayout'
+import { Page } from './styles'
 
 export function HomePage() {
+  const { t } = useTranslation()
   const router = useRouter()
   const [isMounted, setIsMounted] = useState(false)
   const accessToken = useSelector(
@@ -43,21 +48,28 @@ export function HomePage() {
 
   if (!accessToken) {
     return (
-      <Page>
+      <Container>
         <Main>
           <h1>로고</h1>
           <p>캐치 프라이즈</p>
         </Main>
-      </Page>
+      </Container>
     )
   }
 
   return (
-    <Page>
+    <HomeLayout
+      header="ChainG"
+      headerRightButton={
+        <IconButton
+          src="/icons/notice-inactive.svg"
+          alt={t('notice.title')}
+        />
+      }>
       <Main>
         <h1>ChainG 메인 페이지</h1>
         <p>홈이에요</p>
       </Main>
-    </Page>
+    </HomeLayout>
   )
 }
