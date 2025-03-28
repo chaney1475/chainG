f0532e4b39cfaa089b9c14140a3d4fe8795d0b6b
김동열 <ehfql6363@naver.com>
2025-03-28 18:04:03 +0900

    Feat: PDFPathResponse 변환 메서드 추가
    - PDFPathDTO를 PDFPathResponse로 변환하는 from 메서드 구현

--- backend/src/main/java/com/ssafy/chaing/blockchain/controller/response/PDFPathResponse.java
+++ backend/src/main/java/com/ssafy/chaing/blockchain/controller/response/PDFPathResponse.java
@@ -1,5 +1,6 @@
 package com.ssafy.chaing.blockchain.controller.response;
 
+import com.ssafy.chaing.blockchain.service.dto.PDFPathDTO;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -9,4 +10,8 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class PDFPathResponse {
     private String presignedUrl;
+
+    public static PDFPathResponse from(PDFPathDTO dto) {
+        return new PDFPathResponse(dto.getPresignedUrl());
+    }
 }
