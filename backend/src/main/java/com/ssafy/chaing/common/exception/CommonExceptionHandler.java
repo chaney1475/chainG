0ec81d38173822d483a225c3b4b46eae166ef1d6
leedonghyun <skydh507@gmail.com>
2025-03-28 16:10:13 +0900

    Fix : 중복 에러 코드 삭제

--- backend/src/main/java/com/ssafy/chaing/common/exception/CommonExceptionHandler.java
+++ backend/src/main/java/com/ssafy/chaing/common/exception/CommonExceptionHandler.java
@@ -78,22 +78,14 @@ public class CommonExceptionHandler extends ResponseEntityExceptionHandler {
     }
 
     @ExceptionHandler(ServerException.class)
-    public ResponseEntity<BaseResponse<ErrorResponse>> s3BucketException(ServerException e) {
-        log.error("S3 처리 실패: {}", e.getMessage());
+    public ResponseEntity<BaseResponse<ErrorResponse>> serverException(ServerException e) {
+        log.error("외부 서버 처리 실패: {}", e.getMessage());
         ErrorResponse error = ErrorResponse.of(e.getCode(), e.getMessage());
         return ResponseEntity
                 .status(HttpStatus.INTERNAL_SERVER_ERROR)
                 .body(BaseResponse.error(error));
     }
 
-    @ExceptionHandler(ServerException.class)
-    public ResponseEntity<BaseResponse<ErrorResponse>> gptServerException(ServerException e) {
-        log.error("GPT 처리 실패: {}", e.getMessage());
-        ErrorResponse error = ErrorResponse.of(e.getCode(), e.getMessage());
-        return ResponseEntity
-                .status(HttpStatus.INTERNAL_SERVER_ERROR)
-                .body(BaseResponse.error(error));
-    }
 
 }
 
