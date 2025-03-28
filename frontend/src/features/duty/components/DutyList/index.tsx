3c7a00efcf390edeaf2bc22c5d2f79d62f254671
김현래 <gusto917@naver.com>
2025-03-28 14:01:52 +0900

    Merge branch 'dev-fe' of https://lab.ssafy.com/s12-blochain-transaction-sub1/S12P21A302 into feature/duty
    # Conflicts:
    #	frontend/src/features/auth/LoginPage/index.tsx

--- frontend/src/features/duty/components/DutyList/index.tsx
+++ frontend/src/features/duty/components/DutyList/index.tsx
@@ -11,32 +11,7 @@ import { dutyList } from '@/constants/dutyList'
 import { DutyListItem } from '../DutyListItem'
 import { Container } from './styles'
 
-// 내 하위에 있는 style을 쓰겠다
-
-interface LoginForm {
-  emailAddress: string
-  password: string
-}
-
 export function DutyList() {
-  const { t } = useTranslation()
-  const router = useRouter()
-  const {
-    register,
-    handleSubmit,
-    formState: { errors },
-  } = useForm<LoginForm>()
-
-  const onSubmit = async (data: LoginForm): Promise<void> => {
-    try {
-      // TODO: 실제 로그인 API 호출 구현
-      console.log('로그인 데이터:', data)
-      router.push('/')
-    } catch (error) {
-      console.error('로그인 실패:', error)
-    }
-  }
-
   return (
     <Container>
       {dutyList.monday.map((duty) => (
--- frontend/src/features/duty/components/WeekSelector/index.tsx
+++ frontend/src/features/duty/components/WeekSelector/index.tsx
@@ -6,50 +6,20 @@ import { useTranslation } from 'react-i18next'
 
 import { useRouter } from 'next/navigation'
 
-import { ConfirmButton, InputBox, TopHeader } from '@/components'
 import { Container, Form, Main } from '@/styles/styles'
 
 // 공용 컴포넌트 쓰겠다. -> from ~~
 
-import {
-  FullMain,
-  Navigator,
-  SignupLinkContainer,
-  StyledLink,
-  SubmitButton,
-} from './styles'
+import { FullMain, Navigator } from './styles'
 
 // 내 하위에 있는 style을 쓰겠다
-
-interface LoginForm {
-  emailAddress: string
-  password: string
-}
-
-export function DutyCreatePage() {
+export function WeekSelector() {
   const { t } = useTranslation()
   const router = useRouter()
-  const {
-    register,
-    handleSubmit,
-    formState: { errors },
-  } = useForm<LoginForm>()
-
-  const onSubmit = async (data: LoginForm): Promise<void> => {
-    try {
-      // TODO: 실제 로그인 API 호출 구현
-      console.log('로그인 데이터:', data)
-      router.push('/')
-    } catch (error) {
-      console.error('로그인 실패:', error)
-    }
-  }
 
   return (
     <Container>
-      <TopHeader title={t('duty.title')} />
-      <FullMain></FullMain>
-      <Navigator></Navigator>
+      <div> 요일 선택 컴포넌트다 </div>
     </Container>
   )
 }
