19de56b98926bb7124d7fc3fc222cf2562c78d9a
mirim <lll123kong@naver.com>
2025-03-28 10:36:39 +0900

    feat: 생활 규칙 수정 알림바 생성성

---
+++ frontend/src/features/lifeRule/components/NoticeBar/styles.ts
@@ -0,0 +1,17 @@
+import styled from '@emotion/styled'
+
+export const NoticeBarContainer = styled.div`
+  background-color: ${({ theme }) => theme.color.background.white};
+  color: ${({ theme }) => theme.color.text.low};
+  padding: 10px;
+  font-size: 12px;
+  text-align: center;
+  width: 100%;
+  border-radius: 8px;
+  height: 46px;
+  gap: 8px;
+  font-family: ${({ theme }) => theme.typography.fonts.paperlogyMedium};
+  border: 1px solid ${({ theme }) => theme.color.border};
+  display: flex;
+  align-items: center;
+`
--- frontend/src/features/lifeRule/index.tsx
+++ frontend/src/features/lifeRule/index.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import React from 'react'
+import React, { useState } from 'react'
 import { useTranslation } from 'react-i18next'
 
 import { useRouter } from 'next/navigation'
@@ -10,17 +10,27 @@ import { TopHeader } from '@/features/lifeRule/components/TopHeader'
 import { Container } from '@/styles/styles'
 
 import { LifeRuleList } from './components/LifeRuleList'
+import { NoticeBar } from './components/NoticeBar'
 import { FullMain, NavigatorBar } from './styles'
 
 export function LifeRulePage() {
   const { t } = useTranslation()
   const router = useRouter()
 
+  const [isUpdated, setIsUpdated] = useState<boolean>(false)
+
+  const handleUpdateRules = () => {
+    setIsUpdated((prevState) => !prevState)
+  }
+
   return (
     <Container>
       <TopHeader title={t('lifeRule.title')} />
       <FullMain>
+        {isUpdated && <NoticeBar message={t('lifeRule.updateMessage')} />}
         <LifeRuleList lifeRuleList={lifeRuleList} />
+        {/* 임시 상태변경 버튼 */}
+        <button onClick={handleUpdateRules}>생활 규칙 수정</button>
       </FullMain>
       <NavigatorBar></NavigatorBar>
     </Container>
