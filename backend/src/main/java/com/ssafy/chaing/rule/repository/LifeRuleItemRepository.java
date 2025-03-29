1552e70784af9164bb039545b725b2f0b65581cd
leedonghyun <skydh507@gmail.com>
2025-03-30 04:45:07 +0900

    Refactor : 반환타입 set 변경

--- backend/src/main/java/com/ssafy/chaing/rule/repository/LifeRuleItemRepository.java
+++ backend/src/main/java/com/ssafy/chaing/rule/repository/LifeRuleItemRepository.java
@@ -3,8 +3,9 @@ package com.ssafy.chaing.rule.repository;
 import com.ssafy.chaing.rule.domain.LifeRuleEntity;
 import com.ssafy.chaing.rule.domain.LifeRuleItemEntity;
 import java.util.List;
+import java.util.Set;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface LifeRuleItemRepository extends JpaRepository<LifeRuleItemEntity, Long> {
-    List<LifeRuleItemEntity> findAllByLifeRule(LifeRuleEntity lifeRule);
+    Set<LifeRuleItemEntity> findAllByLifeRule(LifeRuleEntity lifeRule);
 }
