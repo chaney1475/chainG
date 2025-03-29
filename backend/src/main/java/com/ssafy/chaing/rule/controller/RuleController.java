eed136335ca3c8a3d34681aac0a3980751d2b954
leedonghyun <skydh507@gmail.com>
2025-03-29 23:05:49 +0900

    Feat : 카테고리 추천 API 및 코드 컨벤션 적용

--- backend/src/main/java/com/ssafy/chaing/rule/controller/RuleController.java
+++ backend/src/main/java/com/ssafy/chaing/rule/controller/RuleController.java
@@ -3,6 +3,9 @@ package com.ssafy.chaing.rule.controller;
 
 import com.ssafy.chaing.auth.domain.UserPrincipal;
 import com.ssafy.chaing.common.schema.BaseResponse;
+import com.ssafy.chaing.recommend.Request.RecommendRequest;
+import com.ssafy.chaing.recommend.response.RecommendResponse;
+import com.ssafy.chaing.recommend.service.RecommendService;
 import com.ssafy.chaing.rule.controller.request.LifeRuleApproveRequest;
 import com.ssafy.chaing.rule.controller.request.LifeRuleFormRequest;
 import com.ssafy.chaing.rule.controller.request.LifeRuleUpdateRequest;
@@ -33,12 +36,14 @@ import org.springframework.web.bind.annotation.RestController;
 public class RuleController {
 
     private final RuleService ruleService;
+    private final RecommendService recommendService;
 
     // 생활룰 전체 페이지 생성
     @PostMapping
-    public ResponseEntity<BaseResponse<LifeRuleResponse>> createLifeRule(@RequestBody LifeRuleFormRequest body,
-                                                                         @AuthenticationPrincipal UserPrincipal principal) {
-
+    public ResponseEntity<BaseResponse<LifeRuleResponse>> createLifeRule(
+            @RequestBody LifeRuleFormRequest body,
+            @AuthenticationPrincipal UserPrincipal principal
+    ) {
         LifeRuleResponse lifeRule = ruleService.createLifeRule(body, principal.getId());
         return ResponseEntity.ok(BaseResponse.success(lifeRule));
     }
@@ -46,15 +51,18 @@ public class RuleController {
     // 생활룰 전체 페이지 조회
     @GetMapping
     public ResponseEntity<BaseResponse<LifeRuleResponse>> getLifeRule(
-            @AuthenticationPrincipal UserPrincipal principal) {
+            @AuthenticationPrincipal UserPrincipal principal
+    ) {
         LifeRuleResponse lifeRules = ruleService.getLifeRules(principal.getId());
         return ResponseEntity.ok(BaseResponse.success(lifeRules));
     }
 
     // 생활룰 수정
     @PostMapping("/update-temp")
-    public ResponseEntity<BaseResponse<List<LifeRuleUpdateDto>>> updateLifeRule(@RequestBody LifeRuleUpdateRequest body,
-                                                                                @AuthenticationPrincipal UserPrincipal principal) {
+    public ResponseEntity<BaseResponse<List<LifeRuleUpdateDto>>> updateLifeRule(
+            @RequestBody LifeRuleUpdateRequest body,
+            @AuthenticationPrincipal UserPrincipal principal
+    ) {
         List<LifeRuleUpdateDto> lifeRuleUpdates = ruleService.updateRules(body, principal.getId());
         return ResponseEntity.ok(BaseResponse.success(lifeRuleUpdates));
     }
@@ -62,27 +70,28 @@ public class RuleController {
     //생활룰 수정 사항 조회
     @GetMapping("/update-temp")
     public ResponseEntity<BaseResponse<List<LifeRuleUpdateDto>>> getUpdateLifeRule(
-            @AuthenticationPrincipal UserPrincipal principal) {
+            @AuthenticationPrincipal UserPrincipal principal
+    ) {
         List<LifeRuleUpdateDto> updateLifeRule = ruleService.getUpdateLifeRule(principal.getId());
         return ResponseEntity.ok(BaseResponse.success(updateLifeRule));
     }
 
     // 생활 룰 수정 승인/거부
     @PostMapping("/approved")
-    public ResponseEntity<BaseResponse<Void>> approveUpdateForm(@RequestBody LifeRuleApproveRequest body,
-                                                                @AuthenticationPrincipal UserPrincipal principal) {
+    public ResponseEntity<BaseResponse<Void>> approveUpdateForm(
+            @RequestBody LifeRuleApproveRequest body,
+            @AuthenticationPrincipal UserPrincipal principal
+    ) {
         ruleService.approveLifeRule(body, principal.getId());
         return ResponseEntity.ok(BaseResponse.success(null));
     }
 
-    // TODO : GPT 카테고리 추천 API 추가.
     @PostMapping("/category")
-    public ResponseEntity<BaseResponse<RecommendCategoryResponse>> recommendCategory(
-            @RequestBody RecommendCategoryRequest body,
-            @AuthenticationPrincipal UserPrincipal principal) {
-
-        return ResponseEntity.ok(BaseResponse.success(null));
+    public ResponseEntity<BaseResponse<RecommendResponse>> recommendCategory(
+            @RequestBody RecommendRequest body
+    ) {
+        RecommendResponse response = recommendService.recommendLifeRuleCategory(body);
+        return ResponseEntity.ok(BaseResponse.success(response));
     }
-
 
 }
