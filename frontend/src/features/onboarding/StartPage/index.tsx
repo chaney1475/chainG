bbcb25119e6f081073097f796f68319e82940744
JEmilyKoo <Jemilykoo@gmail.com>
2025-03-31 00:02:18 +0900

    feat: create onboarding page

--- frontend/src/features/onboarding/StartPage/index.tsx
+++ frontend/src/features/onboarding/StartPage/index.tsx
@@ -1,7 +1,130 @@
 'use client'
 
-import { Container } from './styles'
+import { useState } from 'react'
+import { useTranslation } from 'react-i18next'
+
+import Image from 'next/image'
+import { useRouter } from 'next/navigation'
+
+import {
+  BottomSheet,
+  CardButton,
+  ConfirmButton,
+  IconButton,
+  TitleHeader,
+} from '@/components'
+import { HeaderButton } from '@/components/TopHeader/styles'
+import { BottomContainer, Container, HeaderContainer } from '@/styles/styles'
+import { CardItem } from '@/types/ui'
+
+import { ImageContainer, Main } from './styles'
 
 export function StartPage() {
-  return <Container>온보딩이에요</Container>
+  const router = useRouter()
+  const { t } = useTranslation()
+  const [step, setStep] = useState(0)
+  const [isBottomSheetOpen, setIsBottomSheetOpen] = useState(false)
+
+  const stepContent = [
+    {
+      id: 'blockchain',
+      title: t('onboarding.blockChain'),
+      image: '/images/onboarding/onboarding-blockchain.png',
+    },
+    {
+      id: 'transfer',
+      title: t('onboarding.transfer'),
+      image: '/images/onboarding/onboarding-transfer.png',
+    },
+    {
+      id: 'budget',
+      title: t('onboarding.budget'),
+      image: '/images/onboarding/onboarding-budget.png',
+    },
+    {
+      id: 'awkward',
+      title: t('onboarding.awkward'),
+      image: '/images/onboarding/onboarding-awkward.png',
+    },
+    {
+      id: 'start',
+      title: t('onboarding.start'),
+      image: '/images/onboarding/onboarding-start.png',
+    },
+  ]
+
+  const cardItems: CardItem[] = [
+    {
+      url: '/group/create/createRoom',
+      image: '/images/group/group-create.svg',
+      title: t('onboarding.create.title'),
+      description: t('onboarding.create.description'),
+    },
+    {
+      url: '/group/join/inviteCode',
+      image: '/images/group/group-join.svg',
+      title: t('onboarding.join.title'),
+      description: t('onboarding.join.description'),
+    },
+  ]
+  // 현재 step의 id가 start면 confirm 버튼 의 레이블을 onBoarding.confirm 변경
+  const isConfirmButtonVisible = stepContent[step].id === 'start'
+
+  const handleNext = () => {
+    if (isConfirmButtonVisible) {
+      setIsBottomSheetOpen(true)
+    } else {
+      setStep(step + 1)
+    }
+  }
+  const handleBack = () => {
+    if (step > 0) {
+      setStep(step - 1)
+    } else {
+      router.back()
+    }
+  }
+
+  return (
+    <Container>
+      <HeaderContainer>
+        <IconButton
+          src="/icons/arrow-left.svg"
+          alt={t('icon.back')}
+          onClick={handleBack}
+        />
+        {'ChainG'}
+        <HeaderButton />
+      </HeaderContainer>
+      <Main>
+        <TitleHeader title={stepContent[step].title} />
+        <ImageContainer>
+          <Image
+            src={stepContent[step].image}
+            alt={stepContent[step].title}
+            fill
+            style={{ objectFit: 'cover' }}
+          />
+        </ImageContainer>
+      </Main>
+
+      <BottomContainer>
+        <ConfirmButton
+          onClick={handleNext}
+          label={isConfirmButtonVisible ? t('onboarding.confirm') : t('next')}
+        />
+      </BottomContainer>
+      <BottomSheet
+        open={isBottomSheetOpen}
+        onOpenChange={setIsBottomSheetOpen}
+        snapPoints={{
+          MIN: 0.1,
+          MID: 0.5,
+          MAX: 0.5,
+        }}>
+        <HeaderContainer>{t('onboarding.title')}</HeaderContainer>
+        <CardButton cardItems={cardItems} />
+      </BottomSheet>
+    </Container>
+  )
 }
