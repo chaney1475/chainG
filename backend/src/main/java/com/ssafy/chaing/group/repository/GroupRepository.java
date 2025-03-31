93d639ebb002695c79bfbd7196fa8d062c3776b2
chaney1475 <chaney2225@gmail.com>
2025-03-31 14:06:43 +0900

    feat: Group 조회시 멤버 정보까지 함께 조회

--- backend/src/main/java/com/ssafy/chaing/group/repository/GroupRepository.java
+++ backend/src/main/java/com/ssafy/chaing/group/repository/GroupRepository.java
@@ -3,6 +3,7 @@ package com.ssafy.chaing.group.repository;
 import com.ssafy.chaing.group.domain.GroupEntity;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
@@ -11,4 +12,13 @@ public interface GroupRepository extends JpaRepository<GroupEntity, Long> {
     Optional<GroupEntity> findByGroupCode(String groupCode);
 
     Optional<GroupEntity> findByIdAndGroupCode(Long id, String groupCode);
+
+    @Query("""
+                SELECT DISTINCT g
+                FROM GroupEntity g
+                LEFT JOIN FETCH g.members m
+                LEFT JOIN FETCH m.user
+                WHERE g.id = :groupId
+            """)
+    Optional<GroupEntity> findWithMembersAndUsersById(Long groupId);
 }
