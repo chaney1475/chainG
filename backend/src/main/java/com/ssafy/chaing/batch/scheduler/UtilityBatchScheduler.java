647076b372bfe27651f890b5a5c07a9ef3f1dbcb
김동열 <ehfql6363@naver.com>
2025-04-01 08:57:34 +0900

    Feat: 공과금 배치 스케줄러 활성화
    - @Scheduled 어노테이션의 주석 해제하여 공과금 배치 스케줄러 활성화

--- backend/src/main/java/com/ssafy/chaing/batch/scheduler/UtilityBatchScheduler.java
+++ backend/src/main/java/com/ssafy/chaing/batch/scheduler/UtilityBatchScheduler.java
@@ -30,7 +30,7 @@ public class UtilityBatchScheduler {
     private final JobLauncher jobLauncher;
     private final Job utilityBillingStatementJob;
 
-//    @Scheduled(cron = "0 0 9 * * MON", zone = "Asia/Seoul") // UTC 기준 월요일 0시 0분 0초
+    @Scheduled(cron = "0 0 9 * * MON", zone = "Asia/Seoul") // UTC 기준 월요일 0시 0분 0초
     public void runUtilityBillingJob() {
         try {
             // Job 실행 시 파라미터 전달 (동일 파라미터로 재실행 방지 및 실행 기록 구분용)
