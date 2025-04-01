350932495fe640b32a1d89586136f7ad786d90a5
leedonghyun <skydh507@gmail.com>
2025-04-01 16:07:00 +0900

    Refactor : 알림 카테고리 추가

--- backend/src/main/java/com/ssafy/chaing/notification/domain/NotificationCategory.java
+++ backend/src/main/java/com/ssafy/chaing/notification/domain/NotificationCategory.java
@@ -1,5 +1,5 @@
 package com.ssafy.chaing.notification.domain;
 
 public enum NotificationCategory {
-    RENT, UTILITY, RULE, GENERAL, GROUP, CONTRACT, DUTY, PAYMENT
+    RENT, UTILITY, RULE, GENERAL, GROUP, CONTRACT, DUTY, PAYMENT, LIVING_BUDGET
 }
