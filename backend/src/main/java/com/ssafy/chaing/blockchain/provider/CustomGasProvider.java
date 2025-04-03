281897df87275e9268b2ce9141e977df7f0ed264
김동열 <ehfql6363@naver.com>
2025-04-03 21:06:38 +0900

    Refactor: 가스 가격 및 가스 한도 설정 방식 개선
    - 가스 가격을 GWEI 단위로 변환하여 설정
    - 커스텀 가스 한도를 4,500,000으로 변경

--- backend/src/main/java/com/ssafy/chaing/blockchain/provider/CustomGasProvider.java
+++ backend/src/main/java/com/ssafy/chaing/blockchain/provider/CustomGasProvider.java
@@ -3,11 +3,13 @@ package com.ssafy.chaing.blockchain.provider;
 import java.math.BigInteger;
 import org.web3j.tx.gas.ContractGasProvider;
 import org.web3j.tx.gas.DefaultGasProvider;
+import org.web3j.utils.Convert;
 
 public class CustomGasProvider extends DefaultGasProvider implements ContractGasProvider {
 
     // 네트워크의 최소 가스 팁 캡 요구사항에 맞추어 설정 (25000000000 wei 이상)
-    private static final BigInteger CUSTOM_GAS_PRICE = BigInteger.valueOf(100000000000L);
+    private static final BigInteger CUSTOM_GAS_PRICE = Convert.toWei("100", Convert.Unit.GWEI).toBigInteger();
+    private static final BigInteger CUSTOM_GAS_LIMIT = BigInteger.valueOf(4_500_000L);
 
     @Override
     public BigInteger getGasPrice(String contractFunc) {
@@ -16,6 +18,6 @@ public class CustomGasProvider extends DefaultGasProvider implements ContractGas
 
     @Override
     public BigInteger getGasLimit(String contractFunc) {
-        return super.getGasLimit(contractFunc);
+        return CUSTOM_GAS_LIMIT;
     }
 }
