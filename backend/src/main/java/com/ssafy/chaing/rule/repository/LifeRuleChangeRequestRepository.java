b5216e3ea68eca9eada5a9c375e0dc516000ec79
leedonghyun <skydh507@gmail.com>
2025-04-03 13:05:36 +0900

    Feat:  승인 안한 user 조회 쿼리 추가

--- backend/src/main/java/com/ssafy/chaing/rule/repository/LifeRuleChangeRequestRepository.java
+++ backend/src/main/java/com/ssafy/chaing/rule/repository/LifeRuleChangeRequestRepository.java
@@ -18,14 +18,26 @@ public interface LifeRuleChangeRequestRepository extends JpaRepository<LifeRuleC
     @EntityGraph(attributePaths = {"changeItems"})
     @Lock(LockModeType.PESSIMISTIC_WRITE)
     @Query("""
-           SELECT r
-           FROM LifeRuleChangeRequestEntity r
-           WHERE r.lifeRule = :lifeRule
-             AND r.status = :status
-           """)
+            SELECT r
+            FROM LifeRuleChangeRequestEntity r
+            WHERE r.lifeRule = :lifeRule
+              AND r.status = :status
+            """)
     Optional<LifeRuleChangeRequestEntity> findWithLockByLifeRuleAndStatus(
             @Param("lifeRule") LifeRuleEntity lifeRule,
             @Param("status") ChangeRequestStatus status);
 
     Optional<LifeRuleChangeRequestEntity> findByLifeRule(LifeRuleEntity lifeRule);
+
+    @Query("""
+            SELECT r FROM LifeRuleChangeRequestEntity r
+            JOIN FETCH r.lifeRule lr
+            JOIN FETCH lr.lifeRuleUsers lru
+            JOIN FETCH lru.user u
+            WHERE lr.group.id = :groupId AND r.status = :status
+            """)
+    Optional<LifeRuleChangeRequestEntity> findProgressingRequestWithUsersByGroupId(
+            @Param("groupId") Long groupId,
+            @Param("status") ChangeRequestStatus status
+    );
 }
