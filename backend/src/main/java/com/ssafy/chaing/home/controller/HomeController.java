806990235330c2c7b2a4c713e748d7d3e9802633
서채연 <chaney11@naver.com>
2025-04-03 17:35:12 +0900

    feat: 테스트용 payment 생성 api

--- backend/src/main/java/com/ssafy/chaing/home/controller/HomeController.java
+++ backend/src/main/java/com/ssafy/chaing/home/controller/HomeController.java
@@ -2,6 +2,9 @@ package com.ssafy.chaing.home.controller;
 
 import com.ssafy.chaing.auth.domain.UserPrincipal;
 import com.ssafy.chaing.common.schema.BaseResponse;
+import com.ssafy.chaing.contract.service.dto.ContractDTO;
+import com.ssafy.chaing.home.controller.request.TestIntegrationService;
+import com.ssafy.chaing.home.controller.request.TestPaymentRequest;
 import com.ssafy.chaing.home.controller.response.HomeOverviewResponse;
 import com.ssafy.chaing.home.service.HomeFacade;
 import com.ssafy.chaing.home.service.command.HomeOverviewCommand;
@@ -10,6 +13,8 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -20,17 +25,30 @@ import org.springframework.web.bind.annotation.RestController;
 public class HomeController {
 
     private final HomeFacade homeFacade;
+    private final TestIntegrationService integrationService;
 
     @GetMapping
     public ResponseEntity<BaseResponse<HomeOverviewResponse>> getHomeOverview(
             @AuthenticationPrincipal UserPrincipal principal
     ) {
-
-        System.out.println("principal.getId() = " + principal.getId());
         HomeOverviewDTO dto = homeFacade.getHomeOverview(new HomeOverviewCommand(principal.getId()));
         HomeOverviewResponse response = HomeOverviewResponse.from(dto);
 
         return ResponseEntity.ok()
                 .body(BaseResponse.success(response));
     }
+
+    @PostMapping("/integration-scenario")
+    public ResponseEntity<BaseResponse<ContractDTO>> getHomeOverview(
+            @RequestBody TestPaymentRequest body
+
+    ) {
+        ContractDTO contractDTO = integrationService.createTestPayment(
+                body
+        );
+
+        return ResponseEntity.ok()
+                .body(BaseResponse.success(contractDTO));
+    }
+
 }
