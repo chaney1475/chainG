f5e80a867947428a303bd477b57314c4972a3c54
JEmilyKoo <Jemilykoo@gmail.com>
2025-04-06 23:34:03 +0900

    feat:  api 수정

--- frontend/src/apis/api.ts
+++ frontend/src/apis/api.ts
@@ -6,7 +6,7 @@ import { ApiErrorResponse, ApiResponse } from '@/types/api'
 import { handleDefaultError } from '@/utils/error/handleDefaultError'
 
 const api = axios.create({
-  baseURL: `https://chaing.site/api/v1`,
+  baseURL: `/api/v1`,
   // baseURL: `${process.env.NEXT_PUBLIC_FRONTEND_SCHEME}://${process.env.NEXT_PUBLIC_FRONTEND_HOST}${process.env.NEXT_PUBLIC_FRONTEND_PATH}`,
   timeout: 5000,
   headers: {
