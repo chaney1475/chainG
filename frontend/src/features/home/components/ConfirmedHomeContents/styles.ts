729a97e5294b3403c225359c406f27d2d325f4fa
JEmilyKoo <Jemilykoo@gmail.com>
2025-04-07 21:31:03 +0900

    fix: 홈 ui 간격 수정

--- frontend/src/features/home/components/ConfirmedHomeContents/styles.ts
+++ frontend/src/features/home/components/ConfirmedHomeContents/styles.ts
@@ -1,7 +1,7 @@
 import styled from '@emotion/styled'
 
 export const Container = styled.div`
-  padding: 20px;
+  padding: 20px 0;
   width: 100%;
   background-color: ${({ theme }) => theme.color.background.white};
   height: 100%;
