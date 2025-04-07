8a24d3c236593c1e5a19beb7c5a25157e3446d25
구정은 <jemilykoo@gmail.com>
2025-04-07 12:46:21 +0900

    feat: 공과금 switch 처리 추가

--- frontend/src/components/Card/index.tsx
+++ frontend/src/components/Card/index.tsx
@@ -6,13 +6,21 @@ import Image from 'next/image'
 
 import { createCard } from '@/apis/fintech'
 import { useAppSelector, useIsLeader } from '@/hooks'
-import { setCardConfirm, updateUtility } from '@/store/slices/contractSlice'
 import {
+  setCardConfirm,
+  setUseUtilityCard,
+  updateUtility,
+} from '@/store/slices/contractSlice'
+import {
+  DefaultContainer,
+  Label,
   ShowCenterBox,
-  SlimContainer,
+  TitleContainer,
   ValidationContainer,
   ValidationMessage,
 } from '@/styles/styles'
+
+import { ToggleSwitch } from '../ToggleSwitch'
 
 interface AccountInputProps {
   value?: string | null
@@ -28,6 +36,9 @@ export function Card({ onChange }: AccountInputProps) {
   const cardConfirm = useAppSelector((state) => state.contract.cardConfirm)
   const utility = useAppSelector(
     (state) => state.contract.contractRequest.utility,
+  )
+  const useUtilityCard = useAppSelector(
+    (state) => state.contract.useUtilityCard,
   )
   const rent = useAppSelector((state) => state.contract.contractRequest.rent)
   const isLeader = useIsLeader()
@@ -65,25 +76,29 @@ export function Card({ onChange }: AccountInputProps) {
     setButtonText(content)
   }, [cardConfirm, t])
   return (
-    <SlimContainer>
-      <ShowCenterBox
-        onClick={() => setNext(true)}
-        isDisabled={disabled}>
-        {buttonText}
-      </ShowCenterBox>
-      {!isLeader && !cardConfirm && (
-        <ValidationContainer>
-          <Image
-            src={`/icons/validation-false.svg`}
-            alt={'message'}
-            width={14}
-            height={14}
-          />
-          <ValidationMessage isValid={false}>
-            {t('contract.utility.validation.leaderOnly')}
-          </ValidationMessage>
-        </ValidationContainer>
+    <>
+      {useUtilityCard && (
+        <DefaultContainer>
+          <ShowCenterBox
+            onClick={() => setNext(true)}
+            isDisabled={disabled}>
+            {buttonText}
+          </ShowCenterBox>
+          {!isLeader && !cardConfirm && (
+            <ValidationContainer>
+              <Image
+                src={`/icons/validation-false.svg`}
+                alt={'message'}
+                width={14}
+                height={14}
+              />
+              <ValidationMessage isValid={false}>
+                {t('contract.utility.validation.leaderOnly')}
+              </ValidationMessage>
+            </ValidationContainer>
+          )}
+        </DefaultContainer>
       )}
-    </SlimContainer>
+    </>
   )
 }
