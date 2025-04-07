ad44f4e8f7143230aa3840d2d2dca9a74389332e
김현래 <gusto917@naver.com>
2025-04-07 22:37:42 +0900

    feat: rent에 step 컴포넌트 구현

--- frontend/src/features/pledge/rent/components/Step/index.tsx
+++ frontend/src/features/pledge/rent/components/Step/index.tsx
@@ -1,13 +1,100 @@
 'use client'
 
-import { BoxContainer } from '../../../styles'
-import { ContentContainer } from './styles'
+import { useEffect } from 'react'
 
-export function Graph() {
+import { UserItem } from '@/components/UserItem'
+import { useAppSelector } from '@/hooks/useAppSelector'
+import { BudgetStatus } from '@/types/budget'
+import { User } from '@/types/user'
+
+import { BoxContainer } from '../../../styles'
+import {
+  BarContainer,
+  ContentContainer,
+  StatusIcon,
+  StepContainer,
+  StepItem,
+  TopDescription,
+} from './styles'
+
+interface UserList {
+  user: User
+  month: monthPaid[]
+}
+
+interface monthPaid {
+  month: number
+  finalStatus: BudgetStatus
+}
+
+export function Step() {
+  const rentInfo = useAppSelector((state) => state.pledge.rent)
+  const userId = useAppSelector((state) => state.user.user.id)
+  const user = rentInfo?.currentMonth.find((item) => item.userId === userId)
+  const group = useAppSelector((state) => state.group.group.members)
+
+  useEffect(() => {
+    const date = new Date().getDate()
+    const dutDate = rentInfo?.dueDate || 0
+    const exceedDueDate: boolean = dutDate < date
+
+    rentInfo?.monthList.slice(0, 6).forEach((item) => {
+      item.piadUserIds?.forEach((paidUser) => {
+        userList
+          .find((user) => user.user.id === paidUser)
+          ?.month.push({
+            month: Number(item.month.slice(5)),
+            finalStatus: 'complete',
+          })
+      })
+      item.debtUserIds.forEach((debtUser) => {
+        userList
+          .find((user) => user.user.id === debtUser)
+          ?.month.push({
+            month: Number(item.month.slice(5)),
+            finalStatus: exceedDueDate ? 'debt' : 'expected',
+          })
+      })
+    }) // todo: 6개까지만 가져오도록 한정하기
+  }, [])
+
+  const userList: UserList[] = group.map((item) => {
+    return {
+      user: item,
+      month: [],
+    }
+  })
+
+  console.log('group', group)
+  console.log('rentInfo', rentInfo)
+  console.log('userList', userList)
+
   return (
     <>
       <BoxContainer>
-        <ContentContainer></ContentContainer>
+        <ContentContainer>
+          <TopDescription>전체 납부 현황 </TopDescription>
+
+          <StepContainer>
+            {userList.map((item) => (
+              <StepItem key={item.user.id}>
+                <UserItem
+                  user={item.user}
+                  variant="bar"
+                  size="small"
+                />
+                <BarContainer>
+                  {item.month.map((month) => (
+                    <StatusIcon
+                      variant={month.finalStatus}
+                      key={month.month}
+                    />
+                  ))}
+                </BarContainer>
+              </StepItem>
+            ))}
+          </StepContainer>
+        </ContentContainer>
       </BoxContainer>
     </>
   )
--- frontend/src/features/pledge/rent/components/Step/styles.ts
+++ frontend/src/features/pledge/rent/components/Step/styles.ts
@@ -1,10 +1,70 @@
 import styled from '@emotion/styled'
 
+import { BudgetStatus } from '@/types/budget'
+
 export const ContentContainer = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
   justify-content: center;
   width: 100%;
+  gap: 30px;
 `
 
+export const TopDescription = styled.div`
+  width: 100%;
+  ${({ theme }) => theme.typography.styles.name};
+  color: ${({ theme }) => theme.color.text.disabled};
+`
+
+export const StepContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  width: 100%;
+  gap: 16px;
+`
+
+export const StepItem = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-between;
+  width: 100%;
+  gap: 16px;
+`
+
+export const BarContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-between;
+  height: 14px;
+  width: 100%;
+  border-radius: 24px;
+  background-color: ${({ theme }) => theme.color.secondary};
+`
+
+export const StatusIcon = styled.div<{ variant: BudgetStatus }>`
+  width: 14px;
+  height: 14px;
+  border-radius: 50%;
+
+  ${({ variant, theme }) => {
+    switch (variant) {
+      case 'complete':
+        return `
+          background-color: ${theme.color.text.confirm};
+        `
+      case 'debt':
+        return `
+          background-color: ${theme.color.text.distructive};
+        `
+      case 'expected':
+        return `
+          background-color: ${theme.color.primary};
+        `
+    }
+  }}
+`
