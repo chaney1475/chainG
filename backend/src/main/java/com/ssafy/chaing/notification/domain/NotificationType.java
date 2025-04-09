5fce5977426148a348d21114808c52e9d8db1f03
leedonghyun <skydh507@gmail.com>
2025-04-09 13:07:33 +0900

    Refactor : 생활룰 알림 내용 수정

--- backend/src/main/java/com/ssafy/chaing/notification/domain/NotificationType.java
+++ backend/src/main/java/com/ssafy/chaing/notification/domain/NotificationType.java
@@ -5,12 +5,10 @@ import lombok.Getter;
 @Getter
 public enum NotificationType {
 
-    LIFE_RULE_CREATED("새로운 생활룰이 생성되었어요!", "함께 정한 규칙을 확인해보세요."),
-    LIFE_RULE_APPROVED("생활룰이 확정되었어요!", "확정된 새로운 규칙을 확인해보세요."),
-    LIFE_RULE_UPDATE_REQUESTED("생활룰 수정 요청이 있어요!", "함께 정한 규칙을 검토하고 승인해주세요."),
-    LIFE_RULE_REJECTED("생활룰 수정이 거절되었어요!", "다시 내용을 확인해보고 재요청할 수 있어요.");
-
-
+    LIFE_RULE_CREATED("새로운 생활 규칙이 생성되었어요!", "함께 정한 규칙을 확인해보세요."),
+    LIFE_RULE_APPROVED("생활 규칙이 확정되었어요!", "확정된 새로운 규칙을 확인해보세요."),
+    LIFE_RULE_UPDATE_REQUESTED("생활 규칙에 수정 요청이 있어요!", "함께 정한 규칙을 검토하고 승인해주세요."),
+    LIFE_RULE_REJECTED("생활 규칙 수정이 거절되었어요!", "다시 내용을 확인해보고 재요청할 수 있어요.");
 
 
     private final String title;
