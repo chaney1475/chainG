9475419b7018cc908b76e9dd9d4e54bb94e78005
leedonghyun <skydh507@gmail.com>
2025-04-09 14:32:50 +0900

    Fix: 생활룰 승인 수정

--- backend/src/main/java/com/ssafy/chaing/rule/repository/LifeRuleChangeRequestRepository.java
+++ backend/src/main/java/com/ssafy/chaing/rule/repository/LifeRuleChangeRequestRepository.java
@@ -5,7 +5,6 @@ import com.ssafy.chaing.rule.domain.LifeRuleChangeRequestEntity;
 import com.ssafy.chaing.rule.domain.LifeRuleEntity;
 import jakarta.persistence.LockModeType;
 import java.util.Optional;
-import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Lock;
 import org.springframework.data.jpa.repository.Query;
@@ -15,7 +14,7 @@ public interface LifeRuleChangeRequestRepository extends JpaRepository<LifeRuleC
 
     Optional<LifeRuleChangeRequestEntity> findByLifeRuleAndStatus(LifeRuleEntity lifeRule, ChangeRequestStatus status);
 
-    @EntityGraph(attributePaths = {"changeItems"})
+    //    @EntityGraph(attributePaths = {"changeItems"})
     @Lock(LockModeType.PESSIMISTIC_WRITE)
     @Query("""
             SELECT r
