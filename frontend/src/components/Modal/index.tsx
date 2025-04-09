c57528d454aeb103f6592287e582132e015fd671
구정은 <jemilykoo@gmail.com>
2025-04-09 15:07:57 +0900

    design: 아이콘 4차 수정
    (cherry picked from commit c7ffdbadf92347203af5b1ce357ecb49d69351d1)

--- frontend/src/components/Modal/index.tsx
+++ frontend/src/components/Modal/index.tsx
@@ -17,6 +17,7 @@ interface ModalProps {
   open: boolean
   onOpenChange: (open: boolean) => void
   onConfirm: () => void
+  onCancel?: () => void
   title?: string
   description?: string
   confirmText?: string
@@ -35,6 +36,7 @@ export function Modal({
   children,
   disablePrev = false,
   image,
+  onCancel,
 }: ModalProps) {
   return (
     <Dialog.Root
@@ -70,6 +72,7 @@ export function Modal({
                 <ModalConfirmButton
                   label={'cancel'}
                   variant={'prev'}
+                  onClick={onCancel ?? (() => {})}
                 />
               </Dialog.Close>
             )}
